51cb850c170d46fa8b3eb83b50c12c03889816e0
Dominik Schwind <dschwind@lostfocus.de>
2012-04-13 11:42:19 +0200

    Example

---
+++ example.config.php
@@ -0,0 +1,10 @@
+<?php
+	date_default_timezone_set('Europe/Berlin');
+
+	$config = array(
+		'username' => 'username',
+		'password' => 'password',
+		'lat' => '50.859102626350605',
+		'long' => '7.000688910484314',
+		);
+?>
